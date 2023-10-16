15437f033399af0bba93150d484c649c3da5c83b
vikash kumar <singhvikash533@gmail.com>
2023-10-16 14:50:29 +0530

    firstcommit

---
+++ src/project01/Prog1.java
@@ -0,0 +1,10 @@
+package project01;
+
+public class Prog1 {
+
+	public static void main(String[] args) {
+		System.out.println("My name is vikash");
+		System.out.println("My name is vikash kumar");
+	}
+
+}
