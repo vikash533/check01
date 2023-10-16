6c7477f0470196be8a90453c10826a6ec3356c55
vikash kumar <singhvikash533@gmail.com>
2023-10-16 15:00:10 +0530

    branch1 commit

--- src/project01/Prog1.java
+++ src/project01/Prog1.java
@@ -5,6 +5,7 @@ public class Prog1 {
 	public static void main(String[] args) {
 		System.out.println("My name is vikash");
 		System.out.println("My name is vikash kumar");
+		System.out.println("i am tester");
 	}
 
 }
